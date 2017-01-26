aa80b872b20bcec40aabb44ad7af0c1b081533e2
DAi <alexandr.dermenji@gmail.com>
2017-01-26 12:34:27 +0200

    Added requirements.txt

--- src/prayerlistapp/urls.py
+++ src/prayerlistapp/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),	
-	url(r'^settings/$', views.settings, name='settings'),
+    url(r'^$', views.index, name='index'),	
+    url(r'^settings/$', views.settings, name='settings'),
     url(r'^settings/password/$', views.password, name='password'),
     url(r'^now/$', views.current_datetime),
 ]
