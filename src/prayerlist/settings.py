16a9d35e4d90afc325cf10ebcfd75b05863dbcb4
DAi <alexandr.dermenji@gmail.com>
2017-03-16 16:53:18 +0200

    Added CachedStaticFilesStorage. refs #17

--- src/prayerlist/settings.py
+++ src/prayerlist/settings.py
@@ -155,6 +155,8 @@ LOCALE_PATHS = (
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
+
 # Messages settings
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
